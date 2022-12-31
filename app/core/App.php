e29be53bf83313a14a9c00eec28fd078bfffbda3
darulannas <darulannas7@gmail.com>
2022-12-31 22:12:33 +0700

    add routing

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,20 @@
 <?php
 
 class App {
+
+    //method routing
     public function __construct() {
-        echo "OKE!";
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    //methode routing
+    public function parseURL() {
+        if( isset($_GET["url"])) {
+            $url = rtrim($_GET["url"], "/");
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode("/", $url);
+            return $url;
+        }
     }
 }
