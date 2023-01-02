9c3b9faca1ee1e1bfb159b98edbfbeef19f60b3b
darulannas <darulannas7@gmail.com>
2023-01-02 23:55:27 +0700

    add database wrapper 1

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -22,27 +22,19 @@ class Mahasiswa_model
     //         "jurusan" => "Sistem Informasi"
     //     ]
     // ];
-    //database handler
-    private $dbh;
-    //statement
-    private $stmt;
+
+    private $table = "mahasiswa";
+    private $db;
 
     public function __construct()
     {
-        //data source name
-        $dsn = "mysql:host=localhost;dbname=phpmvc";
-
-        try {
-            $this->dbh = new PDO($dsn, "root", "");
-        } catch (PDOException $e) {
-            die($e->getMessage());
-        }
+        $this->db = new Database;
     }
+
 
     public function getAllMahasiswa()
     {
-        $this->stmt = $this->dbh->prepare("SELECT * FROM mahasiswa");
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->db->query("SELECT * FROM" . $this->table);
+        return $this->db->resultSet();
     }
 }
